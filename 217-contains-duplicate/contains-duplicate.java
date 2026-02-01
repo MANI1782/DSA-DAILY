8df35a8f5cab42a58a2677bf4db5c74b30537be9
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-01 18:48:34 +0530

    Time: 15 ms (64.96%) | Memory: 93.7 MB (31.31%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,19 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // 1. A HashSet to store every number we've encountered
-        Set<Integer> seen = new HashSet<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            // 2. The Value: "Is this specific number already in my bag?"
-            if (seen.contains(nums[i])) {
-                return true; // Found a match! Alarm goes off, function ends.
+        int i=0;
+        Set<Integer> freq=new HashSet<>();
+        for(int high=0;high<nums.length;high++){
+            if(freq.contains(nums[high])){
+              return true;
+            };
+            freq.add(nums[high]);
             }
-            
-            // 3. If it's a new number, put it in the bag
-            seen.add(nums[i]);
-        }
         
-        // 4. If we finish the loop, it means every number was unique
         return false;
     }
 }
