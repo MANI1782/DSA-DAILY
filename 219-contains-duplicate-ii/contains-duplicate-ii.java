3b348e5437f9dd0b6d876b139e071e7481592b7b
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-02 23:13:49 +0530

    Time: 22 ms (92.43%) | Memory: 90.7 MB (37.58%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // We use a Set to act as our "sliding window" of size k
-        Set<Integer> window = new HashSet<>();
-        int low = 0;
-
-        for (int high = 0; high < nums.length; high++) {
-            // 1. If the number is already in our window, we found a nearby duplicate!
-            if (window.contains(nums[high])) {
+ 
+        int low=0;
+        Map<Integer,Integer> freq=new HashMap<>();
+        for(int high=0;high<nums.length;high++){
+            if(freq.containsKey(nums[high])){
+                int diff=high-freq.get(nums[high]);
+                if(diff<=k){
                 return true;
             }
 
-            // 2. Add the current number to the window
-            window.add(nums[high]);
-
-            // 3. This is where your 'low' logic comes in:
-            // If the window size exceeds k, remove the oldest element
-            if (window.size() > k) {
-                window.remove(nums[low]);
-                low++;
             }
-        }
-
-        return false;
+            freq.put(nums[low],high);
+            low++;
+           
+        
+         }
+    return false;
     }
 }
