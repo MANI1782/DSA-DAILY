c463b8a7a64f03ae6505c5dcc97347ae6a7bcddb
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-03 22:28:01 +0530

    Time: 8 ms (17.68%) | Memory: 48.1 MB (10.49%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,16 +1,15 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums); // Line them up: 0, 1, 2, 3...
+       
         
-        // Check every spot
-        for (int i = 0; i < nums.length; i++) {
-            // If the number doesn't match the index, the index is missing!
-            if (nums[i] != i) {
-                return i;
-            }
+       
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+           if(nums[i]!=i){
+            return i;
+           }
         }
-        
-        // If we checked everything and all matched, the missing number is the last one (n)
         return nums.length;
+
     }
 }
