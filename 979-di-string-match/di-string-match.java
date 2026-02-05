86d1f1d28b89e947ccf5f0656084d068932329b3
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-05 23:09:28 +0530

    Time: 5 ms (6.59%) | Memory: 47.6 MB (6.89%) - LeetSync

--- 979-di-string-match/di-string-match.java
+++ 979-di-string-match/di-string-match.java
@@ -1,25 +1,19 @@
 class Solution {
     public int[] diStringMatch(String s) {
-        int n = s.length();
-        int[] ans = new int[n + 1];
-        
-        int left = 0;      // Smallest available
-        int right = n;     // Largest available
-        
-        for (int i = 0; i < n; i++) {
-            if (s.charAt(i) == 'I') {
-                ans[i] = left;
+        int[] ans=new int[s.length()+1];
+        int left=0;
+        int right=s.length();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='I'){
+                ans[i]=left;
                 left++;
-            } else {
-                ans[i] = right;
+            }else {
+                ans[i]=right;
                 right--;
             }
         }
-        
-        // Final Step: left and right will meet here. 
-        // Put the last remaining number in the last slot.
-        ans[n] = left; 
-        
+        ans[s.length()]=left;
+
         return ans;
     }
 }
