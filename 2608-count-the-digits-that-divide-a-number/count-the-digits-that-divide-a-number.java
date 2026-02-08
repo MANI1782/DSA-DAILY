9dbaeb2e26f508505bf7ded7a840898c6d7fbc67
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-08 17:01:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (13.67%) - LeetSync

--- 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.java
+++ 2608-count-the-digits-that-divide-a-number/count-the-digits-that-divide-a-number.java
@@ -1,19 +1,16 @@
 class Solution {
     public int countDigits(int num) {
-        int count = 0;
-
-        // We use a 'temp' variable so we don't lose the original value of 'num'
-        for (int temp = num; temp > 0; temp /= 10) {
-            // 1. Get the last digit
-            int lastDigit = temp % 10;
+        int count=0;
+        int temp=num;
+        while(temp>0){
+            int lastdigit=temp%10;
             
-            // 2. Check if the original 'num' is divisible by this digit
-            // (num % lastDigit == 0) means there is no remainder
-            if (num % lastDigit == 0) {
+            if(num%lastdigit==0){
                 count++;
             }
+            temp=temp/10;
         }
-        
         return count;
-    }
+
+}
 }
