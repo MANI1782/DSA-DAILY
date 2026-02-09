c38e567ea33d360c90aa74110aa411cfeec76794
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-10 00:41:20 +0530

    Time: 3 ms (19.60%) | Memory: 47.5 MB (29.11%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int minans=nums[0];
+        int maxans=nums[0];
+        int ans=nums[0];
+        if(nums.length==1){
+            return nums[0];
+        }
+        for(int i=1;i<nums.length;i++){
+           int v1=nums[i];
+           int v2=maxans*nums[i];
+           int v3=minans*nums[i];
+           minans=Math.min(v1,Math.min(v2,v3));
+           maxans=Math.max(v1,Math.max(v2,v3));
+           ans=Math.max(ans,Math.max(minans,maxans));
+            
+        }
+        return ans;
+
+    }
+}
