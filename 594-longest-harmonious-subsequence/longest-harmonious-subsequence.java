9bc72495dc7a879ad1aab355b656f1d77d157834
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-14 13:23:00 +0530

    Time: 16 ms (89.58%) | Memory: 47.8 MB (95.79%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -3,7 +3,7 @@ class Solution {
         Arrays.sort(nums);
         int low=0;
         int res=0;
-        for(int high=0;high<nums.length;high++){
+        for(int high=1;high<nums.length;high++){
             while(nums[high]-nums[low]>1){
                 low++;
             }if(nums[high]-nums[low]==1){
