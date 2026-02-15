bb6ab2371c442062c951ca7d018c7de8ed31799e
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-15 20:22:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (38.60%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -2,4 +2,4 @@ class Solution {
     public boolean isStrictlyPalindromic(int n) {
         return false;
     }
-}
+}//bandya//haaa 1 word answer solve maad medium level mada//hyange//nan madla en logic illaok
