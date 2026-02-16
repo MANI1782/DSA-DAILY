29771809343a29242535e282433ab11cae2844ae
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-16 15:42:25 +0530

    Time: 4 ms (53.39%) | Memory: 46.3 MB (99.03%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,25 +1,20 @@
 class Solution {
-      static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-     }
+      
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> ans= new HashMap<>();
-        
+        Map<Integer,Integer> ans=new HashMap<>();
+
         for(int i=0;i<nums.length;i++){
-            ans.put(nums[i],i);  
+            ans.put(nums[i],i);
         }
         for(int i=0;i<nums.length;i++){
-           int compliment=target-nums[i];
-           if(ans.containsKey(compliment) && ans.get(compliment)!=i){
-            return new int[]{i,ans.get(compliment)};
-           }
-
+            int compliment=target-nums[i];
+            if(ans.containsKey(compliment) && i!=ans.get(compliment)){
+                return new int[]{i,ans.get(compliment)};
+            }
         }
         return new int[]{};
+
+    
+
     }
 }
