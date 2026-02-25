d5e0c58904b9deff2987a2813a2bc8a26c3704b7
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-25 22:18:18 +0530

    Time: 25 ms (46.30%) | Memory: 49 MB (34.70%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,25 +1,20 @@
 class Solution {
-     static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-     }
+   
     public int subarraySum(int[] nums, int k) {
-     Map<Integer,Integer> ans=new HashMap<>();
-     ans.put(0,1);
-     int prefix=0;
-     int count=0;
-     for(int i=0;i<nums.length;i++){
-        prefix=prefix+nums[i];
-        if(ans.containsKey(prefix-k)){
-            count=count+ans.get(prefix-k);
+        int prefix=0;
+        int count=0;
+        Map<Integer,Integer> ans=new HashMap<>();
+        ans.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            prefix=prefix+nums[i];
+            int sum=prefix-k;
+            if(ans.containsKey(sum)){
+                count=count+ans.get(sum);
+            }
+            ans.put(prefix,ans.getOrDefault(prefix,0)+1);
         }
-        ans.put(prefix,ans.getOrDefault(prefix,0)+1);
-     }
-     return count;
+        return count;
+  
         
     }
 }
