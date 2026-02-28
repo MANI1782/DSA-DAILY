b0310ec3197f4fc97cd0bf8b7b87cad80cf2b37e
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-02-28 22:21:45 +0530

    Time: 9 ms (38.62%) | Memory: 49 MB (72.41%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -5,7 +5,7 @@ class Solution{
     int start1=intervals[0][0];
     int end1=intervals[0][1];
 
-    for(int i=0;i<intervals.length;i++){
+    for(int i=1;i<intervals.length;i++){
         int start2=intervals[i][0];
         int end2=intervals[i][1];
         if(end1>=start2){
