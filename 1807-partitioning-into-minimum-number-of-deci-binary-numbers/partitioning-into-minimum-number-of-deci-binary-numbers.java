1bcc324928b26441a130cdfe1673307600bb28e6
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-01 13:23:47 +0530

    Time: 5 ms (89.94%) | Memory: 47.7 MB (55.09%) - LeetSync

--- 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.java
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -7,6 +7,9 @@ class Solution {
             if(digit>max){
                 max=digit;
             }
+             if(max == 9) {
+                break;
+            }
         }
         return max;
 
