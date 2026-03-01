32d15df8fc1522fed98f9f93b4ba29e9a30dd982
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-01 13:23:06 +0530

    Time: 5 ms (89.94%) | Memory: 47.8 MB (36.60%) - LeetSync

---
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minPartitions(String n) {
+        int max=0;
+        char[] arr=n.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            int digit=arr[i]-'0';
+            if(digit>max){
+                max=digit;
+            }
+        }
+        return max;
+
+        
+    }
+}
