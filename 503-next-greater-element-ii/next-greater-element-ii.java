c5e92b15facaab13f64d0d3cba8bed7b9674c66a
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-05 15:06:41 +0530

    Time: 19 ms (16.68%) | Memory: 47.9 MB (84.55%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        List<Integer> ans=new ArrayList<>();
+        int n=nums.length;
+        Stack<Integer> stack=new Stack<>();
+        for(int i=nums.length*2;i>=0;i--){
+
+           while(!stack.isEmpty() && stack.peek()<=nums[i%n]){
+            stack.pop();
+           }
+           if(stack.isEmpty()){
+            ans.add(-1);
+           }else{
+            ans.add(stack.peek());
+           }
+           stack.push(nums[i%n]);
+        }
+        Collections.reverse(ans);
+        int[] res=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+                res[i]=ans.get(i);
+        }
+        return res;
+    }
+    
+}
