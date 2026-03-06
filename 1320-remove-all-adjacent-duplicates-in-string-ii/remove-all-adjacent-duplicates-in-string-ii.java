10c3cd5f814ea321e678762ec97c6301e9e0bf54
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-06 23:52:20 +0530

    Time: 30 ms (48.44%) | Memory: 47.1 MB (71.86%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,39 +1,29 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        Stack<Character> stack=new Stack<>();
-        int count=0;
+        Stack<int[]> stack=new Stack<>();
         for(int i=0;i<s.length();i++){
             char left=s.charAt(i);
-            
-            if(!stack.isEmpty() && stack.peek()==left){
-                count++;
-                
-            }else{    
-                count=1;
+            if(!stack.isEmpty() && left==stack.peek()[0]){
+                stack.peek()[1]++;
+            }else{
+                stack.push(new int[]{left,1});
             }
-            stack.push(left);
-            if(count==k){
-                while(count>0){
-                    stack.pop();
-                    count--;
-                }
-                
-                if(!stack.isEmpty()){
-                    char top=stack.peek();
-                    int j=stack.size()-1;
-                    while(j>=0 && stack.get(j)==top){
-                        count++;
-                        j--;
-                    }
-                }
+            if(stack.peek()[1]==k){
+                stack.pop();
             }
 
         }
         StringBuilder sb=new StringBuilder();
         for(int i=0;i<stack.size();i++){
-            sb.append(stack.get(i));
+            int[] pair=stack.get(i);
+            int j=0;
+            while(j<pair[1]){
+                sb.append((char)pair[0]);
+                j++;
+            }
         }
         return sb.toString();
+       
         
     }
 }
