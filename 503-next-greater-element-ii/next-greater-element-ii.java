48c4a40f3550bd5ff568a3eb209ae67e5ba7aff7
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-07 13:39:19 +0530

    Time: 20 ms (15.80%) | Memory: 47.8 MB (87.66%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -4,23 +4,22 @@ class Solution {
         int n=nums.length;
         Stack<Integer> stack=new Stack<>();
         for(int i=nums.length*2;i>=0;i--){
-
-           while(!stack.isEmpty() && stack.peek()<=nums[i%n]){
-            stack.pop();
-           }
-           if(stack.isEmpty()){
-            ans.add(-1);
-           }else{
-            ans.add(stack.peek());
-           }
-           stack.push(nums[i%n]);
+            while(!stack.isEmpty() && stack.peek() <=nums[i%n]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans.add(-1);
+            }else{
+                ans.add(stack.peek());
+            }
+            stack.push(nums[i%n]);
         }
         Collections.reverse(ans);
-        int[] res=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-                res[i]=ans.get(i);
-        }
-        return res;
+      int[] res=new int[nums.length];
+      for(int i=0;i<nums.length;i++){
+        res[i]=ans.get(i);
+      }
+      return res;
     }
     
 }
