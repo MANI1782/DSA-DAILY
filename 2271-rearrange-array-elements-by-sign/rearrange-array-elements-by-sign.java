e0b828af65f44310fa83d1626a93c75fcc419a0f
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-10 00:33:12 +0530

    Time: 3 ms (99.59%) | Memory: 175.5 MB (67.19%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,33 +1,49 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-       List<Integer> pos=new ArrayList<>();
-       List<Integer> neg=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>0){
-                pos.add(nums[i]);
-            }
-        }
-          for(int i=0;i<nums.length;i++){
-            if(nums[i]<0){
-                neg.add(nums[i]);
-            }
-        }
+    //    List<Integer> pos=new ArrayList<>();
+    //    List<Integer> neg=new ArrayList<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         if(nums[i]>0){
+    //             pos.add(nums[i]);
+    //         }
+    //     }
+    //       for(int i=0;i<nums.length;i++){
+    //         if(nums[i]<0){
+    //             neg.add(nums[i]);
+    //         }
+    //     }
 
-        int[] ans=new int[nums.length];
-        int i=0;
-        int j=0;
-        int k=0;
+    //     int[] ans=new int[nums.length];
+    //     int i=0;
+    //     int j=0;
+    //     int k=0;
     
-        while(i<pos.size() && j<neg.size()){
-                ans[k]=pos.get(i);
-                k++;
-                i++;
-                ans[k]=neg.get(j);
-                j++;
-                k++;
+    //     while(i<pos.size() && j<neg.size()){
+    //             ans[k]=pos.get(i);
+    //             k++;
+    //             i++;
+    //             ans[k]=neg.get(j);
+    //             j++;
+    //             k++;
+    //     }
+    //     return ans;
+    int[] res=new int[nums.length];
+    int even=0;
+    int odd=1;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]>0){
+            res[even]=nums[i];
+            even=even+2;
+           
+        }else{
+            res[odd]=nums[i];
+             odd=odd+2;
+            
         }
-        return ans;
-
         
     }
+    return res;
+    
+    }
+
 }
