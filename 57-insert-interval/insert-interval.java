64479559ed1be8ae23fd90e62169ee4ca3e8e4de
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-16 01:24:05 +0530

    Time: 3 ms (15.51%) | Memory: 47.2 MB (51.53%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -2,32 +2,36 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int start1=newInterval[0];
         int end1=newInterval[1];
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
     List<List<Integer>> ans=new ArrayList<>();
 
-        for(int i=0;i<intervals.length;i++){
-            int start2=intervals[i][0];
-            int end2=intervals[i][1];
+    for(int i=0;i<intervals.length;i++){
+        int start2=intervals[i][0];
+        int end2=intervals[i][1];
 
-            if(end2<start1){
-                ans.add(Arrays.asList(start2,end2));
-            }
-            else if(start2>end1){
+        if(end2<start1){
+            ans.add(Arrays.asList(start2,end2));
+
+
+        }
+        else if(end1<start2) {
                 ans.add(Arrays.asList(start1,end1));
                 start1=start2;
                 end1=end2;
-            }else{
-                start1=Math.min(start1,start2);
-                end1=Math.max(end1,end2);
-            }
-            
+         
+        }else{
+            start1=Math.min(start1,start2);
+            end1=Math.max(end1,end2);
         }
-        ans.add(Arrays.asList(start1,end1));
-        int[][] answer=new int[ans.size()][2];
-        for(int i=0;i<ans.size();i++){
-            answer[i][0]=ans.get(i).get(0);
-            answer[i][1]=ans.get(i).get(1);
-        }
-        return answer;
-        
+    }
+    ans.add(Arrays.asList(start1,end1));
+    int[][] res=new int[ans.size()][2];
+    for(int k=0;k<ans.size();k++){
+        res[k][0]=ans.get(k).get(0);
+        res[k][1]=ans.get(k).get(1);
+    }
+    return res;
+
+      
     }
 }
