38b0c107f1d9282796a02927d551e67a0bb9ffdf
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-28 17:06:23 +0530

    Time: 19 ms (42.65%) | Memory: 47.4 MB (86.95%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.java
+++ 2486-most-frequent-even-element/most-frequent-even-element.java
@@ -1,24 +1,34 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-        int max=-1;
-        int ans=-1;
-
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        
-        for(int num:map.keySet()){
-            if(num%2==0){
-                int freq=map.get(num);
-                if(freq>max || (freq==max && num<ans)){
-                 max=freq;
-                 ans=num;   
-                    
-                }
+        HashMap <Integer,Integer> frq = new HashMap<>() ;
+        for (int i = 0 ; i < nums.length ; i++){
+            if (nums[i] % 2 == 0){
+            if (frq.containsKey(nums[i])){
+                frq.put(nums[i] , frq.get(nums[i]) + 1 ) ;
+            }else{
+                frq.put(nums[i] ,1);
+            }
             }
         }
-        return ans;
-        
+        int maxValue = 0 ; 
+        for (int value : frq.values()){
+            if (value > maxValue){
+                maxValue = value ;
+            }
+        }
+        ArrayList<Integer> maxFrq = new ArrayList<>() ; 
+        for (Map.Entry<Integer,Integer> entry : frq.entrySet()){
+                if (entry.getValue() == maxValue){
+                    maxFrq.add(entry.getKey());
+                }     
+         }
+        if (maxFrq.size()>1){
+         Collections.sort(maxFrq );
+         return maxFrq.get(0) ; 
+        }else if (maxFrq.size() == 0){
+            return -1 ;
+        }else{
+            return maxFrq.get(0) ; 
+        }
     }
 }
