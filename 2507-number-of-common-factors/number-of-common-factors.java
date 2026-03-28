b482ee6be137468cbda9a4fec365bf88e4ad83ba
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-28 09:18:39 +0530

    Time: 1 ms (56.60%) | Memory: 42.1 MB (69.45%) - LeetSync

---
+++ 2507-number-of-common-factors/number-of-common-factors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int count=0;
+        int i=1;
+        while(i<=a && i<=b){
+            if(a%i==0 && b%i==0){
+                count++;
+                i++;
+              
+            }else{
+                i++;
+               
+            }
+        }
+        return count;
+        
+    }
+}
