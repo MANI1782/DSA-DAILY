737abd3fda6d43c25ead4e4347b44f0f2766795f
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-03-30 23:24:20 +0530

    Time: 1 ms (95.72%) | Memory: 43.2 MB (42.92%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+
+        int i=0;
+        int j=0;
+        StringBuilder sb=new StringBuilder();
+        while(i<word1.length() && j<word2.length()){
+            char left=word1.charAt(i);
+            char right=word2.charAt(j);
+
+            sb.append(left);
+            i++;
+            sb.append(right);
+            j++;
+
+        }
+        while(i<word1.length()){
+            char left=word1.charAt(i);
+            sb.append(left);
+            i++;
+        }
+         while(j<word2.length()){
+            char left=word2.charAt(j);
+            sb.append(left);
+            j++;
+        }
+        return sb.toString();
+        
+    }
+}
