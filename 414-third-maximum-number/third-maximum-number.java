a40a310161ee876e650988cb3f9e342dbb8ffbe3
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-04-01 23:08:05 +0530

    Time: 8 ms (18.50%) | Memory: 46.6 MB (10.56%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,47 +1,27 @@
 class Solution {
     public int thirdMax(int[] nums) {
-       
-    Set<Integer> set=new HashSet<>();
-    for(int num:nums){
-        set.add(num);
-    }
-    int k=3;
-    PriorityQueue<Integer> pq = new PriorityQueue<>();
-    for(int num:set){
-        pq.add(num);
-        if(pq.size()>3){
-            pq.poll();
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
-    }
-    if(pq.size()==3){
-        return pq.peek();
-    }else{
-        while(pq.size()>1){
-            pq.poll();
+        int k = 3;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : set) {
+            pq.add(num);
+            if (pq.size() > 3) {
+                pq.poll();
+            }
         }
-        return pq.peek();
+        if (pq.size() == 3) {
+            return pq.peek();
+        } else {
+            while (pq.size() > 1) {
+                pq.poll();
+            }
+            return pq.peek();
+        }
+
     }
 
-    
-        
-    }
-
-//     class Solution {
-//     public int thirdMax(int[] nums) {
-//         PriorityQueue<Integer> heap = new PriorityQueue<>(Collections.reverseOrder());
-//         for(int num : nums){
-//             if(!heap.contains(num))
-//                 heap.add(num);
-//         }
-//         int k = 3;
-//         if(heap.size() <k){
-//             return heap.poll();
-//         }
-//         while(k>1 && !heap.isEmpty()){
-//             heap.poll();
-//             k--;
-//         }
-//         return heap.poll();
-//     }
-// }
 }
