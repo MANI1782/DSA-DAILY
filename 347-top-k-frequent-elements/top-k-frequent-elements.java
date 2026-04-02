b90d9707f4232f4a7775c1e7506658663ab10f1c
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-04-02 23:44:26 +0530

    Time: 14 ms (70.57%) | Memory: 47.4 MB (86.14%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -5,46 +5,38 @@ class Solution {
             (a,b)->{
                 if(a.first!=b.first){
                     return a.first-b.first;
-                    
                 }
                 return a.second-b.second;
             }
         );
-            
-      
-    
 
-        Map<Integer,Integer> map=new HashMap<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
+
         for(int num:nums){
             map.put(num,map.getOrDefault(num,0)+1);
         }
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            int key=entry.getKey();
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
             int value=entry.getValue();
-            Pair current=new Pair(value,key);   
+            int key=entry.getKey();
+            Pair current=new Pair(value,key);
 
             if(pq.size()<k){
                 pq.add(current);
-                continue;
-            } else{
-                if(pq.peek().first>current.first){
-                    continue;
-                }else{
+            }else{
+                if(pq.peek().first<current.first){
                     pq.poll();
                     pq.add(current);
                 }
             }
         }
-        int[] res=new int[k];
+        int res[]=new int[k];
         int i=0;
         while(!pq.isEmpty()){
-            res[i]=pq.peek().second;
-            pq.poll();
+            res[i]=pq.poll().second;
             i++;
         }
         return res;
-
-
 
 
         
