84f83508f469ec173ffeb458b7e295bac0bb8f9f
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-04-05 23:12:44 +0530

    Time: 3 ms (95.30%) | Memory: 46.8 MB (88.96%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.java
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -1,23 +1,23 @@
 class Solution {
     public int[] numberGame(int[] nums) {
 
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for(int num:nums){
+        for (int num : nums) {
             pq.add(num);
         }
 
-        int[] res=new int[nums.length];
-        int i=0;
-        while(!pq.isEmpty()){
-            int poll1=pq.poll();
-            int poll2=pq.poll();
-            res[i]=poll2;
+        int[] res = new int[nums.length];
+        int i = 0;
+        while (!pq.isEmpty()) {
+            int poll1 = pq.poll();
+            int poll2 = pq.poll();
+            res[i] = poll2;
             i++;
-            res[i]=poll1;
+            res[i] = poll1;
             i++;
         }
         return res;
-        
+
     }
 }
