dff170e02b99b6e3b618486fd810126adb2b6832
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-04-14 00:05:54 +0530

    Time: 16 ms (58.15%) | Memory: 50.7 MB (86.73%) - LeetSync

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<Node> pq=new PriorityQueue<>(new Compare());
+        int times=k;
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<matrix.length;i++){
+            
+            pq.add(new Node(matrix[i][0],i,0));
+            
+            
+            
+            
+        }
+       while(times>1){
+        Node node=pq.poll();
+        int col=node.col;
+        if(col+1<m){
+            pq.add(new Node(matrix[node.row][col+1], node.row ,col+1));
+
+        }
+               
+       
+        
+        times--;
+
+       }
+       
+       return pq.poll().value;
+
+        
+    }
+}
+
+class Node{
+    int value;
+    int row;
+    int col;
+    Node(int v,int r,int c){
+        this.value=v;
+        this.row=r;
+        this.col=c;
+    }
+}
+class Compare implements Comparator<Node>{
+    public int compare(Node a,Node b){
+        return Integer.compare(a.value,b.value);
+    }
+}
