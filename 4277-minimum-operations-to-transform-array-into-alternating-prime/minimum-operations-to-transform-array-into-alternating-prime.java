421160418d820a2152387133de4d7de7c4929da7
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-04-18 15:46:02 +0530

    Time: 294 ms (48.84%) | Memory: 93.3 MB (62.82%) - LeetSync

---
+++ 4277-minimum-operations-to-transform-array-into-alternating-prime/minimum-operations-to-transform-array-into-alternating-prime.java
@@ -0,0 +1,43 @@
+    class Solution {
+        public int minOperations(int[] nums) {
+            int i=0;
+            int j=1;
+            int count=0;
+            while(i<nums.length && j<nums.length){
+                int even=nums[i];
+                int odd=nums[j];
+            
+                while(!prime(even)){
+                        count++;
+                        even=even+1;
+                }
+        
+                while(prime(odd)){
+                        count++;
+                        odd=odd+1;
+                }
+                
+                i=i+2;
+                j=j+2;
+            }
+            if(i<nums.length){
+                int even=nums[i];
+                while(!prime(even)){
+                    count++;
+                    even++;
+                }
+            }
+            return count;
+            
+        }
+        boolean prime(int n){
+            if (n <= 1) return false;
+            int count=0;
+            for(int i=2;i*i<=n;i++){
+                if(n%i==0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
