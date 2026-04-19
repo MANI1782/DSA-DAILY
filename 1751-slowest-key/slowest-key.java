9aea75b2b333a5826bd07883c4abac457343ae05
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-04-19 22:23:33 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (31.32%) - LeetSync

--- 1751-slowest-key/slowest-key.java
+++ 1751-slowest-key/slowest-key.java
@@ -1,40 +1,53 @@
 class Solution {
     public char slowestKey(int[] releaseTimes, String keysPressed) {
-        PriorityQueue<Pair> pq=new PriorityQueue<>(
-            (a,b)->{
-                if(a.freq!=b.freq){
-                    return Integer.compare(b.freq,a.freq);
+        // PriorityQueue<Pair> pq=new PriorityQueue<>(
+        //     (a,b)->{
+        //         if(a.freq!=b.freq){
+        //             return Integer.compare(b.freq,a.freq);
                     
-                }
-                return Character.compare(b.ch,a.ch);
+        //         }
+        //         return Character.compare(b.ch,a.ch);
                 
-            }
-        );
-        int sub=0;
-        for(int i=0;i<releaseTimes.length;i++){
-            char ch=keysPressed.charAt(i);
-            int freq=releaseTimes[i]-sub;
-            Pair pair=new Pair(freq,ch);
-            pq.add(pair);
-            sub=releaseTimes[i];
+        //     }
+        // );
+        // int sub=0;
+        // for(int i=0;i<releaseTimes.length;i++){
+        //     char ch=keysPressed.charAt(i);
+        //     int freq=releaseTimes[i]-sub;
+        //     Pair pair=new Pair(freq,ch);
+        //     pq.add(pair);
+        //     sub=releaseTimes[i];
 
-        }
-        Pair pair=pq.poll();
+        // }
+        // Pair pair=pq.poll();
        
 
-        return pair.ch;
+        // return pair.ch;
+        int max=releaseTimes[0];
+        int sub=0;
+        
+        char key=keysPressed.charAt(0);
+        for(int i=1;i<releaseTimes.length;i++){
+            char ch=keysPressed.charAt(i);
+            int ans=releaseTimes[i]-releaseTimes[i-1];
+            if(ans>max|| (ans==max)&& ch>key ) {
+                max=Math.max(max,ans);
+                key=ch;
+            }
+        }
+        return key;
         
     }
 }
-class Pair{
+// class Pair{
     
-    int freq;
-    char ch;
+//     int freq;
+//     char ch;
 
-    Pair(int f,char c){
-        this.freq=f;
-        this.ch=c;
+//     Pair(int f,char c){
+//         this.freq=f;
+//         this.ch=c;
         
         
-    }
-}
+//     }
+// }
