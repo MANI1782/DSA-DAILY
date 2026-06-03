97c30700978b6573bdd756b2778b3d3d28ee5154
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-06-03 21:41:48 +0530

    Time: 4 ms (58.06%) | Memory: 46.4 MB (75.38%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,35 +1,33 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<Character,Integer> freq=new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
         
-        freq.put('I',1);
-        freq.put('V',5);
-        freq.put('X',10);
-        freq.put('L',50);
-        freq.put('C',100);
-        freq.put('D',500);
-        freq.put('M',1000);
         int res = 0;
-        int prev=0;
-        int i = s.length()-1;
+        int prev = 0;
+        int i=s.length()-1;
         while(i>=0){
-            int curr = freq.get(s.charAt(i));
-           if(curr>prev){
-            res=res+curr;
-        
-           }else if(curr<prev){
-            res=res-curr;
-    
-           }
-           else if(curr == prev){
-            res=res+curr;
-           }
-           prev=curr;
-           i--;
+            int curr = map.get(s.charAt(i));  
+            if(curr>prev){
+                res+=curr;
+            }
+            else if(curr<prev){
+                res-=curr;
+            }
+            else if(curr==prev){
+                res+=curr;
+            }
             
+            prev = curr;
+            i--;
         }
-        return res;
-
-     
+    return res;
     }
+    
 }
