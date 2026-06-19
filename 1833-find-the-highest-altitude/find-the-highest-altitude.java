71668746f68ec0dffcc3083d495827fdbf2e169e
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-06-19 12:32:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (55.78%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -1,12 +1,11 @@
 class Solution {
     public int largestAltitude(int[] gain) {
-        long sum = 0;
-        long max = 0;
-        for(int i=0; i<gain.length; i++){ 
-            sum+=gain[i];
-            max = Math.max(max,sum);
-           
-        }
-    return (int)max;    
+      long sum=0;
+      long res=0;
+      for(int num:gain){
+        sum=sum+num;
+        res=Math.max(sum,res);
+      }
+      return (int)res;
     }
 }
