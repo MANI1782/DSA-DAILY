ddcb07d2d042355dcdd2a7a6adfea324fb1cb69e
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-06-29 14:06:43 +0530

    Time: 9 ms (35.39%) | Memory: 48.7 MB (95.47%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,32 +1,32 @@
 class Solution{
     public int[][] merge(int[][] intervals) {
-    List<List<Integer>> ans=new ArrayList<>();
-    Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-    int start1=intervals[0][0];
-    int end1=intervals[0][1];
-
-    for(int i=1;i<intervals.length;i++){
-        int start2=intervals[i][0];
-        int end2=intervals[i][1];
-        if(end1>=start2){
-            start1=start1;
-            end1=Math.max(end1,end2);
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        int start1=intervals[0][0];
+        int end1=intervals[0][1];
+        for(int i=0;i<intervals.length;i++){
+            int start2=intervals[i][0];
+            int end2=intervals[i][1];
+            if(end1>=start2){
+                start1=start1;
+                end1=Math.max(end1,end2);
+            }else{
+                ans.add(Arrays.asList(start1,end1));
+                start1=start2;
+                end1=end2;
+            }
             
-        }else{
-            ans.add(Arrays.asList(start1,end1));
-            start1=start2;
-            end1=end2;
-        }
-    }
-    ans.add(Arrays.asList(start1,end1));
 
-    int[][] answer=new int[ans.size()][2];
-    for(int i=0;i<ans.size();i++){
-        answer[i][0]=ans.get(i).get(0);
-        answer[i][1]=ans.get(i).get(1);
-    }
-    return answer;
-    
+
+        }
+        ans.add(Arrays.asList(start1,end1));
+        int[][] res=new int[ans.size()][2];
+        for(int k=0;k<ans.size();k++){
+            res[k][0]=ans.get(k).get(0);
+            res[k][1]=ans.get(k).get(1);
+        }
+        return res;
+
      
     }
 }
