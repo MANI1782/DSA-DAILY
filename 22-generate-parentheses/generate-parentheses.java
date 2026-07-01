79eba304169c64d5e118e95c87023a73dd655d5e
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-07-01 12:09:37 +0530

    Time: 1 ms (86.78%) | Memory: 44.4 MB (81.23%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> list=new ArrayList<>();
-        parentesis(n,0,0,list,"");
+        parentesis(n,0,0,list,new StringBuilder());
         return list;
 
 
     }
       
-      void parentesis(int n,int start,int end,List<String> ans,String temp){
+      void parentesis(int n,int start,int end,List<String> ans,StringBuilder sb){
             if(start==n && end==n){
-                ans.add(temp);
+                ans.add(sb.toString());
             }
             if(start<n){
-                parentesis(n,start+1,end,ans,temp+'(');
+                sb.append('(');
+                parentesis(n,start+1,end,ans,sb);
+                 sb.deleteCharAt(sb.length()-1);
             }
             if(end<start){
-                parentesis(n,start,end+1,ans,temp+')');
+                sb.append(')');
+                parentesis(n,start,end+1,ans,sb);
+                sb.deleteCharAt(sb.length()-1);
             }
       }
 }
