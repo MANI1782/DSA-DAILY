3d32ef35cb81cde72f9402fd29c50497cc75fb67
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-07-01 15:47:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (20.41%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -7,15 +7,15 @@ class Solution {
     }
 
     void subset(int arr[],int idx,List<List<Integer>> list,List<Integer> curr){
-        if(arr.length==idx){
+        
             list.add(new ArrayList<>(curr));
-            return;
-        }
+         
 
-        curr.add(arr[idx]);
-        subset(arr,idx+1,list,curr);
-        curr.remove(curr.size()-1);
-        subset(arr, idx + 1, list, curr);
+        for(int i=idx;i<arr.length;i++){
+            curr.add(arr[i]);
+            subset(arr,i+1,list,curr);
+            curr.remove(curr.size()-1);
+        }
 
         
         
