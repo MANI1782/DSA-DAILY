3d34892ee12a508787feab5feed2097d83a8b3d2
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-07-03 16:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (12.81%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,73 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr=new ArrayList<>();
+        List<List<Integer>> res=new ArrayList<>();
 
-        for (int a = 1; a <= 9; a++) {
-            if (k == 1) {
-                if (a == n) res.add(Arrays.asList(a));
-                continue;
-            }
-
-            for (int b = a + 1; b <= 9; b++) {
-                if (k == 2) {
-                    if (a + b == n) res.add(Arrays.asList(a, b));
-                    continue;
-                }
-
-                for (int c = b + 1; c <= 9; c++) {
-                    if (k == 3) {
-                        if (a + b + c == n) res.add(Arrays.asList(a, b, c));
-                        continue;
-                    }
-
-                    for (int d = c + 1; d <= 9; d++) {
-                        if (k == 4) {
-                            if (a + b + c + d == n) res.add(Arrays.asList(a, b, c, d));
-                            continue;
-                        }
-
-                        for (int e = d + 1; e <= 9; e++) {
-                            if (k == 5) {
-                                if (a + b + c + d + e == n) res.add(Arrays.asList(a, b, c, d, e));
-                                continue;
-                            }
-
-                            for (int f = e + 1; f <= 9; f++) {
-                                if (k == 6) {
-                                    if (a + b + c + d + e + f == n)
-                                        res.add(Arrays.asList(a, b, c, d, e, f));
-                                    continue;
-                                }
-
-                                for (int g = f + 1; g <= 9; g++) {
-                                    if (k == 7) {
-                                        if (a + b + c + d + e + f + g == n)
-                                            res.add(Arrays.asList(a, b, c, d, e, f, g));
-                                        continue;
-                                    }
-
-                                    for (int h = g + 1; h <= 9; h++) {
-                                        if (k == 8) {
-                                            if (a + b + c + d + e + f + g + h == n)
-                                                res.add(Arrays.asList(a, b, c, d, e, f, g, h));
-                                            continue;
-                                        }
-
-                                        for (int i = h + 1; i <= 9; i++) {
-                                            if (k == 9) {
-                                                if (a + b + c + d + e + f + g + h + i == n)
-                                                    res.add(Arrays.asList(a, b, c, d, e, f, g, h, i));
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
+        track(k,n,1,curr,res,0);
         return res;
+        
+    }
+    void track(int k,int n,int idx,List<Integer> curr,List<List<Integer>> res,int sum){
+        if(curr.size()==k && sum==n){
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        for(int i=idx;i<=9;i++){
+            curr.add(i);
+            sum=sum+i;
+            track(k,n,i+1,curr,res,sum);
+            sum=sum-i;
+            curr.remove(curr.size()-1);
+
+        }
     }
 }
