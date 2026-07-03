07b0a4eb969c3723ca526e1cc5db657b31b5792a
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-07-03 12:50:35 +0530

    Time: 6 ms (73.47%) | Memory: 50.4 MB (5.79%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> curr=new ArrayList<>();
+        track(candidates,0,curr,res,0,target);
+        return res;
+        
+    }
+
+    void track(int nums[],int idx,List<Integer> curr,List<List<Integer>> res,int sum,int target){             // 1 1 
+        
+            if(sum==target){
+                res.add(new ArrayList<>(curr));
+                 return;
+            }
+           
+        
+        for(int i=idx;i<nums.length;i++){
+           if (i > idx && nums[i] == nums[i - 1]){
+            continue;
+           }
+            if (sum + nums[i] > target) break;  
+            else{
+                curr.add(nums[i]);
+                sum=sum+nums[i];
+                track(nums,i+1,curr,res,sum,target);
+                curr.remove(curr.size()-1);
+                sum=sum-nums[i];
+            }
+        }
+    }
+}
