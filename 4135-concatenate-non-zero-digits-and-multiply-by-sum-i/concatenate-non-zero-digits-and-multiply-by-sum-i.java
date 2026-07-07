29f882790e486814a555cfc35580dce6d2ac6b6e
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-07-07 11:59:12 +0530

    Time: 1 ms (99.85%) | Memory: 42.5 MB (83.11%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,23 +1,40 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        long sum=0;
-        if(n==0){
-            return 0;
-        }
-        StringBuilder sb=new StringBuilder();
-        while(n>0){
-            int last=n%10;
-            sum=sum+(long)last;
-            if(last!=0){
-            sb.append(String.valueOf(last));
-            }
-            n=n/10;
-        }
-        sb.reverse();
-        String ans=sb.toString();
-        int res=Integer.parseInt(ans);
-        return sum*(long)res;
+        long sum=sumfind(n);
+        int find=rev(n);
+        return sum*(long)find;
+        
 
         
     }
+    long sumfind(int n){
+        long sum=0;
+        while(n>0){
+            int last=n%10;
+            sum=sum+(long)last;
+            n=n/10;
+        }
+        return sum;
+
+    }
+    int rev(int n){
+        int sum=0;
+        int res=0;
+        while(n>0){
+            int last=n%10;
+            if(last!=0){
+            sum=(sum*10)+last;
+            
+            }
+            n=n/10;
+        }
+        while(sum>0){
+            int last=sum%10;
+            res=(res*10)+last;
+            sum=sum/10;
+        }
+        return res;
+    
+    }
+
 }
