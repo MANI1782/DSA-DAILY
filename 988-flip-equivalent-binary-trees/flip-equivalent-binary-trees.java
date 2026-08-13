8c24cd577b41562d536af06b0012d6178fa1e512
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-08-13 15:10:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (58.44%) - LeetSync

---
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        
+        return check(root1,root2);
+        
+    }
+    boolean check(TreeNode root1,TreeNode root2){
+        if(root1==null && root2==null){
+            return true;
+        }
+        if(root1==null || root2==null){
+            return false;
+        }
+        if(root1.val!=root2.val){
+            return false ;
+        }
+boolean r1=check(root1.left,root2.left) ;
+boolean r2=check(root1.right,root2.right);
+boolean r3=check(root1.left,root2.right);
+boolean r4=check(root1.right,root2.left);
+
+        if(r1 && r2 || r3 && r4){
+            return true;
+        }
+        return false;
+
+    }   
+    
+}
