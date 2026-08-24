a1917ab961ab21f2931b930f1605ee8e1ea2883f
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-08-24 15:05:26 +0530

    Time: 2 ms (95.79%) | Memory: 46 MB (18.44%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,28 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<Integer> ans=new ArrayList<>();
-        combination(candidates,0,0,ans,res,target);
-        return res;
-        
+        List<Integer> curr=new ArrayList<>();
+        List<List<Integer>> list=new ArrayList<>();
+        check(candidates,0,curr,list,0,target);
+        return list;
     }
-    void combination(int arr[],int idx,int sum,List<Integer> dairy,List<List<Integer>> res,int target){
-        if(idx==arr.length){
+    void check(int[] nums,int idx,List<Integer> curr,List<List<Integer>> list,int sum,int target){
+        if(nums.length==idx){
             if(sum==target){
-                res.add(new ArrayList<>(dairy));
+            list.add(new ArrayList<>(curr));
             }
-            return ;
+            return;
         }
-        combination(arr,idx+1,sum,dairy,res,target);
-        if(arr[idx]+sum<=target){
-            dairy.add(arr[idx]);
-
-            sum=sum+arr[idx];
-
-            combination(arr,idx,sum,dairy,res,target);
-            sum=sum-arr[idx];
-            dairy.remove(dairy.size()-1);
-            
+        check(nums,idx+1,curr,list,sum,target);
+        if(nums[idx]+sum<=target){
+            curr.add(nums[idx]);
+            sum=sum+nums[idx];
+            check(nums,idx,curr,list,sum,target);
+            curr.remove(curr.size()-1);
+            sum=sum-nums[idx];
         }
     }
 }
