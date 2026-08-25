c345997934df6ccf7d0b0127bbc154b981957949
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-08-25 09:58:38 +0530

    Time: 2 ms (68.20%) | Memory: 45.4 MB (52.43%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -5,42 +5,15 @@ class Solution {
             set.add(num);
         }
         int num=k;
-        int res=0;
-       
         
-        while(true){
-            if(!set.contains(num)){
-               res=num;
-               break;
-                
-            }
+        while(set.contains(num)){
+           
             num=num+k;
-           
-            
-            
-        }
-        return res;
-        
-    }
-}
-
-
-/* class Solution {
-    public int missingMultiple(int[] nums, int k) {
-        Set<Integer> set=new HashSet<>();
-        for(int num:nums){
-            set.add(num);
-        }
-        int num=k;
-        int res=k;
-        
-        while(set.contains(res)){
-           
-            num=res;
-            res=res*2;
             
         }
         return num;
         
     }
-} */
+}
+
+
