ad7b7e1b5f98ba48b5e4454c5bc7e2416dfdee8b
Manish Moger <130274321+MANI1782@users.noreply.github.com>
2026-08-25 15:19:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (65.58%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<Integer> curr=new ArrayList<>();
         List<List<Integer>> res=new ArrayList<>();
-
-        track(k,n,1,curr,res,0);
+        List<Integer> curr=new ArrayList<>();
+        track(0,1,res,curr,k,n);
         return res;
         
     }
-    void track(int k,int n,int idx,List<Integer> curr,List<List<Integer>> res,int sum){
-        if(curr.size()==k && sum==n){
-            res.add(new ArrayList<>(curr));
+    void track(int sum,int idx,List<List<Integer>> res,List<Integer> curr,int k,int n){
+        if(curr.size()==k){
+            if(sum==n){
+                res.add(new ArrayList<>(curr));
+            }
             return;
         }
         for(int i=idx;i<=9;i++){
-            curr.add(i);
             sum=sum+i;
-            track(k,n,i+1,curr,res,sum);
-            sum=sum-i;
+            curr.add(i);
+            track(sum,i+1,res,curr,k,n);
             curr.remove(curr.size()-1);
-
+            sum=sum-i;
         }
     }
 }
